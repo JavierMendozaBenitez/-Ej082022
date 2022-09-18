b2bc91a2b147cf14cc7a1fed98f88722b806ad7d
JavierMendozaBenitez <javier.mendoza.benitez@gmail.com>
2022-09-18 19:12:34 -0300

    18-09-2022 2do

---
+++ I02 Consultaste el índice/I02 Consultaste el índice/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Entidades;
+
+namespace I02_Consultaste_el_índice
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Libro libro = new Libro();
+
+            libro[2] = "HOLA";
+
+            Console.WriteLine("Indice 0: {0}", libro[0]);
+            Console.WriteLine("Indice 1: {0}", libro[1]);
+            Console.WriteLine("Indice 2: {0}", libro[2]);
+            Console.WriteLine("Indice 3: {0}", libro[3]);
+            Console.WriteLine("Indice 4: {0}", libro[4]);
+
+            Console.ReadKey();
+        }
+    }
+}
